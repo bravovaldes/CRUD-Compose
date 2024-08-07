bdc7ee8fd04efc3dce44128027e8c8dad1036277
bravovaldes <bravomezankou00@gmail.com>
2024-08-06 20:10:56 -0400

    Initial commit

---
+++ app/src/main/java/uqac/dim/daggerhilt/di/AppModule.kt
@@ -0,0 +1,17 @@
+package uqac.dim.daggerhilt.di
+
+import android.content.Context
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import uqac.dim.daggerhilt.MyApplication
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+
+}
